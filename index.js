d53592ab78e706cb135aaa064f65b842a37b8459
CatherineHonors <cathonors@gmail.com>
2022-04-06 13:51:00 -0400

    commit tree dced20af08f707872731cf44e2c8b759be20875e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+let findMatching = (drivers, person) => {
+    let result = drivers.filter(drivers => drivers.toLowerCase() === person.toLowerCase());
+    return result
+}
+
+let fuzzyMatch = (drivers, person) => {
+    //return drivers.filter(drivers => drivers[0], drivers[1] === person)
+    return drivers.filter(drivers => drivers.indexOf(person) === 0)
+        
+}
+
+
+
+pers1 = 'Bobby';
+
